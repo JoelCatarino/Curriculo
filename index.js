ba53d6a76d874e44b97cc43a188affba2c02af5f
Joel Catarino <62433408+JoelCatarino@users.noreply.github.com>
2021-06-03 14:46:37 -0300

    Create Button

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ $(".j_hover").hover(function () {
 }).mouseenter(function () {
     $(this).text("Click Here!");
 }).mouseleave(function () {
-    $(this).text("Download CV!");
+    $(this).text("I'm Sad!");
 });
